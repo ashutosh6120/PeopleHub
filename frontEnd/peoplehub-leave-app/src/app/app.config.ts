f0cbebc3bf2b288b020dd953bacf704bbf9478e5
Ashutosh <ashutosh4064@gmail.com>
2026-06-28 01:47:52 +0530

    add httpclient in leave app

--- frontEnd/peoplehub-leave-app/src/app/app.config.ts
+++ frontEnd/peoplehub-leave-app/src/app/app.config.ts
@@ -1,13 +1,14 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
 import { routes } from './app.routes';
 import { APP_BASE_HREF } from '@angular/common';
+import { provideHttpClient } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
+    provideHttpClient(),
     provideRouter(routes),
     {provide: APP_BASE_HREF, useValue: '/leaves'},
   ]
