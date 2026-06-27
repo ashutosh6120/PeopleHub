c533a31e4734ffbcf0e1877be644093897aebe16
Ashutosh <ashutosh4064@gmail.com>
2026-06-27 17:06:55 +0530

    add route updates and changes

--- frontEnd/peoplehub-leave-app/src/app/app.config.ts
+++ frontEnd/peoplehub-leave-app/src/app/app.config.ts
@@ -2,11 +2,13 @@ import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChang
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { APP_BASE_HREF } from '@angular/common';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes)
+    provideRouter(routes),
+    {provide: APP_BASE_HREF, useValue: '/leaves'},
   ]
 };
--- frontEnd/peoplehub-leave-app/src/app/app.routes.ts
+++ frontEnd/peoplehub-leave-app/src/app/app.routes.ts
@@ -1,6 +1,10 @@
 import { Routes } from '@angular/router';
 import { EmptyRouteComponent } from './empty-route/empty-route.component';
+import { ApplyLeaveComponent } from './components/apply-leave/apply-leave';
+import { LeaveManagementComponent } from './components/leave-management/leave-management';
 
 export const routes: Routes = [
-    { path: '**', component: EmptyRouteComponent}
+    { path: 'apply', component: ApplyLeaveComponent },
+    { path: '',  component: LeaveManagementComponent },
+    { path: '**', component: EmptyRouteComponent }
 ];
