6be4b9860228bc180748af2449014a182c4a37aa
Ashutosh <ashutosh4064@gmail.com>
2026-06-27 16:23:57 +0530

    add routes file

--- frontEnd/peoplehub-employee-app/src/app/app.routes.ts
+++ frontEnd/peoplehub-employee-app/src/app/app.routes.ts
@@ -1,6 +1,13 @@
 import { Routes } from '@angular/router';
 import { EmptyRouteComponent } from './empty-route/empty-route.component';
+import { EmployeeList } from './components/employee-list/employee-list';
+import { EmployeeForm } from './components/employee-form/employee-form';
+import { EmployeeProfile } from './components/employee-profile/employee-profile';
 
 export const routes: Routes = [
+    {path: '', component: EmployeeList },
+    {path: 'add', component: EmployeeForm},
+    {path: 'edit/:id', component: EmployeeForm },
+    {path: 'profile/:id', component: EmployeeProfile},
     { path: '**', component: EmptyRouteComponent}
 ];
